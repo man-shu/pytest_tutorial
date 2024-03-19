negative value fix: scaling gives negative values when std >> 2*mean of simulated data

tests/test_8_check_outlier_removal.py
@@ -4,7 +4,7 @@ import pytest
 import numpy as np
 
 
-@pytest.mark.parametrize("outlier_cutoff_sd", np.linspace(0, 0.15, 5))
+@pytest.mark.parametrize("outlier_cutoff_sd", np.linspace(0.01, 0.15, 5))
 def test_outlier_removal(outlier_cutoff_sd):
     """Whether reaction times only consist of values less than the given cutoff"""
     test_df = generate_test_df(2, 1, 0.8)